1159fb959a10b0da0d7f026588e12d6d879206c3
John Hysong <99369137+johnatticus@users.noreply.github.com>
2024-03-22 08:55:32 -0400

    style changes, images

--- js/script.js
+++ js/script.js
@@ -2,11 +2,11 @@ function userScroll() {
     const navbar = document.querySelector('.navbar');
   
     window.addEventListener('scroll', () => {
-      if (window.scrollY > 50) {
-        navbar.classList.add('bg-dark');
+      if (window.scrollY > 150) {
+        navbar.classList.add('bg-primary');
         navbar.classList.add('navbar-sticky');
       } else {
-        navbar.classList.remove('bg-dark');
+        navbar.classList.remove('bg-primary');
         navbar.classList.remove('navbar-sticky');
       }
     });
